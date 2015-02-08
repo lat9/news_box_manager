f5109c40a416db8abf1836c8ac654e43274d9fa9
lat9 <lat9@vinosdefrutastropicales.com>
2015-02-08 15:19:47 -0500

    Convert german language file to utf-8
    Convert the german-language admin file to utf-8

--- YOUR_ADMIN/includes/languages/german/news_box_manager.php
+++ YOUR_ADMIN/includes/languages/german/news_box_manager.php
@@ -11,10 +11,10 @@
 define('NEWS_BOX_HEADING_TITLE', 'News Box Manager');// JTD
 
 define('TABLE_HEADING_NEWS', 'News');
-define('TABLE_HEADING_SIZE', 'Titel + Inhalt Größe');
+define('TABLE_HEADING_SIZE', 'Titel + Inhalt GrÃ¶ÃŸe');
 define('TABLE_HEADING_NEWS_START', 'Startdatum');
 define('TABLE_HEADING_NEWS_END', 'Enddatum');
-define('TABLE_HEADING_PUBLISHED', 'Veröffentlicht');
+define('TABLE_HEADING_PUBLISHED', 'VerÃ¶ffentlicht');
 define('TABLE_HEADING_STATUS', 'Status');
 define('TABLE_HEADING_ACTION', 'Aktion');
 
@@ -23,11 +23,11 @@ define('TEXT_NEWS_CONTENT', 'News Inhalt:');
 define('TEXT_MORE_NEWS_PAGE', 'Link zur "News" Seite:');
 define('TEXT__NEWS_CONTENT', 'News Inhalt:');
 
-define('TEXT_NEWS_DATE_ADDED', 'Hinzugefügt am:');
-define('TEXT_NEWS_DATE_MODIFIED', 'Verändert am:');
+define('TEXT_NEWS_DATE_ADDED', 'HinzugefÃ¼gt am:');
+define('TEXT_NEWS_DATE_MODIFIED', 'VerÃ¤ndert am:');
 define('TEXT_NEWS_START_DATE', 'News startet am:');
 define('TEXT_NEWS_END_DATE', 'News endet am:');
-define('TEXT_NEWS_PUBLISHED_DATE', 'Veröffentlicht am:');
+define('TEXT_NEWS_PUBLISHED_DATE', 'VerÃ¶ffentlicht am:');
 
 define('TEXT_NEWS_DELETE_INFO', 'Are you sure you want to delete this news?');
 
@@ -36,5 +36,3 @@ define('ERROR_NEWS_CONTENT', 'News content not set!');
 
 define('TEXT_DISPLAY_NUMBER_OF_NEWS', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> news)');
 define('TEXT_NEWS_BOX_MANAGER_INFO', 'The News Box is set to display up to <b>%s</b> bytes (characters) not including HTML tags before link to "News" page is automatically shown. The # can be configured in Layout Settings.<br />To edit or delete news entry, it needs to be in unpublished mode.');
-
-?>
