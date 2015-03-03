046686a82f60a13ab074b1713142e811348439a5
lat9 <lat9@vinosdefrutastropicales.com>
2015-03-03 08:21:32 -0500

    Add SQL uninstall script
    Add the uninstall SQL

---
+++ uninstall_news_box_manager.sql
@@ -0,0 +1,5 @@
+DELETE FROM configuration WHERE configuration_key LIKE 'NEWS_BOX_%';
+DELETE FROM configuration_group WHERE configuration_group_title = 'News Box Manager';
+DROP TABLE IF EXISTS box_news;
+DROP TABLE IF EXISTS box_news_content;
+DELETE FROM admin_pages WHERE page_key IN ('localizationNewsBox', 'configNewsBox');
